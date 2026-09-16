9dea827498026a6a2f070cd64b756c99d3ae4461
Narezzzs <nareswara353@gmail.com>
2026-09-16 20:16:46 +0700

    feat(policy): add EnrollmentPolicy with role-based scope

---
+++ app/policies/enrollment_policy.rb
@@ -0,0 +1,39 @@
+class EnrollmentPolicy < ApplicationPolicy
+  def index?
+    admin? || instructor_of_course?
+  end
+
+  def show?
+    admin? || owner? || instructor_of_course?
+  end
+
+  def create?
+    user&.student?
+  end
+
+  def destroy?
+    admin? || owner?
+  end
+
+  private
+
+  def owner?
+    user.present? && record.user_id == user.id
+  end
+
+  def instructor_of_course?
+    user&.instructor? && record.course.instructor_id == user.id
+  end
+
+  def admin?
+    user&.admin?
+  end
+
+  class Scope < Scope
+    def resolve
+      return scope.all if user&.admin?
+      return scope.joins(:course).where(courses: { instructor_id: user.id }) if user&.instructor?
+      scope.where(user: user)
+    end
+  end
+end
