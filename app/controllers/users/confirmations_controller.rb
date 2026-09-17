aab464795d43e4853299d39453a8a142032c15f2
Narezzzs <nareswara353@gmail.com>
2026-09-17 22:03:43 +0700

    feat(auth): add custom ConfirmationsController with redirect

---
+++ app/controllers/users/confirmations_controller.rb
@@ -0,0 +1,10 @@
+module Users
+  class ConfirmationsController < Devise::ConfirmationsController
+    private
+
+    def after_confirmation_path_for(_resource_name, resource)
+      sign_in(resource)
+      dashboard_path
+    end
+  end
+end
