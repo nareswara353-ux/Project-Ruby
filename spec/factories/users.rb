b04c458bca4b9101492c136c8132892b59212858
Narezzzs <nareswara353@gmail.com>
2026-09-17 22:15:34 +0700

    test(factory): add user factory with role traits

---
+++ spec/factories/users.rb
@@ -0,0 +1,23 @@
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    email { Faker::Internet.unique.email }
+    password { "password123" }
+    password_confirmation { "password123" }
+    role { :student }
+    confirmed_at { Time.current }
+
+    trait :admin do
+      role { :admin }
+    end
+
+    trait :instructor do
+      role { :instructor }
+      bio { Faker::Lorem.paragraph }
+    end
+
+    trait :student do
+      role { :student }
+    end
+  end
+end
