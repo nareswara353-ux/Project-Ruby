d8341ac247464b09fccc4d0d3e87faaf1281beb9
Narezzzs <nareswara353@gmail.com>
2026-09-17 22:03:51 +0700

    feat(auth): add custom PasswordsController with redirect

---
+++ app/controllers/users/passwords_controller.rb
@@ -0,0 +1,9 @@
+module Users
+  class PasswordsController < Devise::PasswordsController
+    private
+
+    def after_sending_reset_password_instructions_path_for(_resource_name)
+      new_user_session_path
+    end
+  end
+end
