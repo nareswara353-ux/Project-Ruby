48545861708e953a6ab6fb85c8f795d3dae6e825
Narezzzs <nareswara353@gmail.com>
2026-09-17 22:04:49 +0700

    feat(policy): add CourseModulePolicy with ownership check

---
+++ app/policies/course_module_policy.rb
@@ -0,0 +1,27 @@
+class CourseModulePolicy < ApplicationPolicy
+  def show?
+    true
+  end
+
+  def create?
+    admin? || instructor_of_course?
+  end
+
+  def update?
+    admin? || instructor_of_course?
+  end
+
+  def destroy?
+    admin? || instructor_of_course?
+  end
+
+  private
+
+  def instructor_of_course?
+    user&.instructor? && record.course.instructor_id == user.id
+  end
+
+  def admin?
+    user&.admin?
+  end
+end
