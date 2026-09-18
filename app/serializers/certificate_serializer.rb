fc26dee3bc6095b8c60a562cc5183df00040ced8
Narezzzs <nareswara353@gmail.com>
2026-09-18 20:52:43 +0700

    feat(serializer): add CertificateSerializer with validation status

---
+++ app/serializers/certificate_serializer.rb
@@ -0,0 +1,13 @@
+class CertificateSerializer < ApplicationSerializer
+  def as_json
+    {
+      id: object.id,
+      code: object.code,
+      issued_at: object.issued_at.iso8601,
+      expires_at: object.expires_at&.iso8601,
+      valid: object.valid?,
+      user: { id: object.user.id, name: object.user.name },
+      course: { id: object.course.id, title: object.course.title, slug: object.course.slug }
+    }
+  end
+end
