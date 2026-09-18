880c4e967cbaf88eb728908e5ed7c3f7ba9ccc7f
Narezzzs <nareswara353@gmail.com>
2026-09-18 20:46:07 +0700

    feat(api): add QuizzesController for API v1

---
+++ app/controllers/api/v1/quizzes_controller.rb
@@ -0,0 +1,28 @@
+module Api
+  module V1
+    class QuizzesController < BaseController
+      before_action :set_course, only: [:index]
+      before_action :set_quiz, only: [:show]
+
+      def index
+        quizzes = @course.quizzes.published
+        render json: QuizSerializer.serialize(quizzes)
+      end
+
+      def show
+        authorize @quiz, :show?
+        render json: QuizSerializer.serialize(@quiz)
+      end
+
+      private
+
+      def set_course
+        @course = Course.find_by!(slug: params[:course_id])
+      end
+
+      def set_quiz
+        @quiz = Quiz.find(params[:id])
+      end
+    end
+  end
+end
