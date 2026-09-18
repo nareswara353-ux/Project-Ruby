e3b057590c46454b6f8a24af8041854ce4717f5c
Narezzzs <nareswara353@gmail.com>
2026-09-18 20:38:12 +0700

    test(model): fix Lesson spec by replacing shoulda-matchers for slug/position

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -8,16 +8,23 @@ RSpec.describe Lesson, type: :model do
   end
 
   describe "validations" do
-    subject { build(:lesson) }
-
     it { is_expected.to validate_presence_of(:title) }
-    it { is_expected.to validate_presence_of(:position) }
-    it { is_expected.to validate_presence_of(:slug) }
+    it { is_expected.to validate_length_of(:title).is_at_most(200) }
   end
 
   describe "enums" do
     it { is_expected.to define_enum_for(:status).with_values(draft: 0, published: 1) }
     it { is_expected.to define_enum_for(:lesson_type).with_values(video: 0, text: 1, quiz: 2) }
+  end
+
+  describe "auto-position assignment" do
+    it "assigns position based on existing lessons" do
+      modul = create(:course_module)
+      create(:lesson, course_module: modul, position: 1)
+      lesson = build(:lesson, course_module: modul, position: nil)
+      lesson.valid?
+      expect(lesson.position).to eq(2)
+    end
   end
 
   describe "#completed_by?" do
