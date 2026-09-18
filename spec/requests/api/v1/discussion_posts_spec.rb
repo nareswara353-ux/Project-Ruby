86960f0d9b9aa5bb6a25ff808826f26d3430a4d0
Narezzzs <nareswara353@gmail.com>
2026-09-18 20:57:43 +0700

    test(api): add auth headers to discussion posts spec

--- spec/requests/api/v1/discussion_posts_spec.rb
+++ spec/requests/api/v1/discussion_posts_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::DiscussionPosts", type: :request do
+  let(:user) { create(:user, :student) }
   let(:course) { create(:course, status: :published) }
-  let(:topic) { create(:discussion_topic, course: course) }
-  let!(:post_record) { create(:discussion_post, discussion_topic: topic) }
+  let(:topic) { create(:discussion_topic, course: course, user: user) }
+  let!(:post_record) { create(:discussion_post, discussion_topic: topic, user: user) }
+
+  let(:auth_headers) { { "Authorization" => "Bearer #{user.api_token}" } }
 
   describe "GET /api/v1/discussion_topics/:discussion_topic_id/discussion_posts" do
     it "returns posts for topic" do
-      get "/api/v1/discussion_topics/#{topic.id}/discussion_posts"
+      get "/api/v1/discussion_topics/#{topic.id}/discussion_posts", headers: auth_headers
       expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body)
       expect(json.size).to eq(1)
@@ -16,7 +19,7 @@ RSpec.describe "Api::V1::DiscussionPosts", type: :request do
 
   describe "GET /api/v1/discussion_posts/:id" do
     it "returns post detail" do
-      get "/api/v1/discussion_posts/#{post_record.id}"
+      get "/api/v1/discussion_posts/#{post_record.id}", headers: auth_headers
       expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body)
       expect(json["author"]).to be_present
