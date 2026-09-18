d11882808366ea413a23f22cd4195b10a5b41305
Narezzzs <nareswara353@gmail.com>
2026-09-18 20:57:39 +0700

    test(api): add auth headers to discussion topics spec

--- spec/requests/api/v1/discussion_topics_spec.rb
+++ spec/requests/api/v1/discussion_topics_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::DiscussionTopics", type: :request do
+  let(:user) { create(:user, :student) }
   let(:course) { create(:course, status: :published) }
-  let!(:topic) { create(:discussion_topic, course: course) }
+  let!(:topic) { create(:discussion_topic, course: course, user: user) }
+
+  let(:auth_headers) { { "Authorization" => "Bearer #{user.api_token}" } }
 
   describe "GET /api/v1/courses/:course_id/discussion_topics" do
     it "returns discussion topics" do
-      get "/api/v1/courses/#{course.slug}/discussion_topics"
+      get "/api/v1/courses/#{course.slug}/discussion_topics", headers: auth_headers
       expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body)
       expect(json.size).to eq(1)
@@ -15,7 +18,7 @@ RSpec.describe "Api::V1::DiscussionTopics", type: :request do
 
   describe "GET /api/v1/discussion_topics/:id" do
     it "returns topic detail with author" do
-      get "/api/v1/discussion_topics/#{topic.id}"
+      get "/api/v1/discussion_topics/#{topic.id}", headers: auth_headers
       expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body)
       expect(json["author"]).to be_present
